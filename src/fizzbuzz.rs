da589466f453f986f44e15a91927b4c682a2e41d
tj19832 <tj19832@gmail.com>
2021-01-20 00:21:08 +0800

    should_return_string_of_number_given_input_is_normal_number,and try pub function

---
+++ src/fizzbuzz.rs
@@ -0,0 +1,14 @@
+pub struct FizzBuzz {
+
+}
+
+impl FizzBuzz {
+    pub fn say_it(input: u32) -> String {
+        return input.to_string();
+    }
+}
+
+
+pub fn say_it_external(input: u32) -> String {
+    return input.to_string();
+}
---
+++ src/main.rs
@@ -0,0 +1,25 @@
+mod fizzbuzz;
+
+#[cfg(test)]
+mod test {
+    use crate::fizzbuzz::FizzBuzz;
+    use crate::fizzbuzz::say_it_external;
+
+    #[test]
+    fn should_return_1_given_input_is_1_try_external() {
+        let result = say_it_external(1);
+
+        assert_eq!("1", result);
+    }
+
+    #[test]
+    fn should_return_string_of_number_given_input_is_normal_number() {
+        let result = FizzBuzz::say_it(1);
+
+        assert_eq!("1", result);
+    }
+
+
+}
+
+fn main() {}
